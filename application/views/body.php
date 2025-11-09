20944a61d0f4de58018ab556b0f8afdad30896bd
RioAdrna <fikarugamerz123@gmail.com>
2025-11-09 22:39:14 +0700

    push awal projek lingkunganku

---
+++ application/views/body.php
@@ -0,0 +1,388 @@
+<!--
+=========================================================
+* Argon Dashboard 3 - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+
+
+<body class="g-sidenav-show   bg-gray-100">
+	<div class="min-height-300 bg-dark position-absolute w-100"></div>
+	<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
+		<div class="sidenav-header">
+			<i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+			<a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
+				<img src="<?= base_url('assets/img/logos/Logo_LingkunganKU-1.png') ?>" width="36px" height="260px" class="navbar-brand-img h-100" alt="main_logo">
+				<span class="ms-1 font-weight-bold">LingkunganKu</span>
+			</a>
+		</div>
+		<hr class="horizontal dark mt-0">
+		<div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+			<ul class="navbar-nav">
+				<?php $current_page = base64_decode($this->input->get("p")); ?>
+				<li class="nav-item">
+					<a class="nav-link <?= ($current_page == "dashboard") ? "active" : "" ?>" href="<?= base_url("?p=" . base64_encode('dashboard')) ?>">
+						<div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+							<i class="fas fa-home text-dark text-sm opacity-10"></i>
+						</div>
+						<span class="nav-link-text ms-1">Dashboard</span>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link <?= ($current_page == "laporan_masalah") ? "active" : "" ?>" href="<?= base_url("?p=" . base64_encode('laporan_masalah')) ?>">
+						<div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+							<i class="fas fa-tasks-alt text-dark text-sm opacity-10"></i>
+						</div>
+						<span class="nav-link-text ms-1">Laporan Masalah</span>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link <?= ($current_page == "status_laporan") ? "active" : "" ?>" href="<?= base_url("?p=" . base64_encode('status_laporan')) ?>">
+						<div class=" icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+							<i class="fas fa-tasks text-dark text-sm opacity-10"></i>
+						</div>
+						<span class="nav-link-text ms-1">Status Laporan</span>
+					</a>
+				</li>
+				<li class="nav-item mt-3">
+					<h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link <?= ($current_page == "profile") ? "active" : "" ?>" href="<?= base_url("?p=" . base64_encode('profile')) ?>">
+						<div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+							<i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
+						</div>
+						<span class="nav-link-text ms-1">Profile</span>
+					</a>
+				</li>
+				<!-- <li class="nav-item">
+					<a class="nav-link <?= ($current_page == "login") ? "active" : "" ?>" href="<?= base_url("?p=" . base64_encode('login')) ?>">
+						<div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+							<i class="ni ni-single-copy-04 text-dark text-sm opacity-10"></i>
+						</div>
+						<span class="nav-link-text ms-1">Sign In</span>
+					</a> -->
+				</li>
+			</ul>
+		</div>
+		<div class="sidenav-footer mx-3 ">
+			<div class="card card-plain shadow-none" id="sidenavCard">
+				<img class="w-50 mx-auto" src="<?= base_url('assets/img/illustrations/icon-documentation.svg') ?>" alt="sidebar_illustration">
+				<div class="card-body text-center p-3 w-100 pt-0">
+					<div class="docs-info">
+						<h6 class="mb-0">Need help?</h6>
+						<!-- <p class="text-xs font-weight-bold mb-0">Please check our docs</p> -->
+					</div>
+				</div>
+			</div>
+			<br>
+			<a class="btn btn-primary mt-3 w-100" href=""><i class="fas fa-sign-out-alt"></i>&nbsp;
+				Logout</a>
+		</div>
+	</aside>
+	<main class="main-content position-relative border-radius-lg ">
+		<!-- Navbar -->
+		<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
+			<div class="container-fluid py-1 px-3">
+				<nav aria-label="breadcrumb">
+					<!-- <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+						<li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
+						<li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
+					</ol>
+					<h6 class="font-weight-bolder text-white mb-0">Dashboard</h6> -->
+				</nav>
+				<div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+					<div class="ms-md-auto pe-md-3 d-flex align-items-center">
+						<div class="input-group">
+							<span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
+							<input type="text" class="form-control" placeholder="Type here...">
+						</div>
+					</div>
+					<ul class="navbar-nav  justify-content-end">
+						<li class="nav-item d-flex align-items-center">
+							<a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
+								<i class="fa fa-sign-out-alt me-sm-1"></i>
+								<span class="d-sm-inline d-none">Log Out</span>
+							</a>
+						</li>
+						<li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+							<a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+								<div class="sidenav-toggler-inner">
+									<i class="sidenav-toggler-line bg-white"></i>
+									<i class="sidenav-toggler-line bg-white"></i>
+									<i class="sidenav-toggler-line bg-white"></i>
+								</div>
+							</a>
+						</li>
+
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<!-- End Navbar -->
+		<div class="container-fluid py-6">
+
+			<div class="container-xxl flex-grow-1 container-p-y">
+				<?php
+				$halaman = base64_decode($this->input->get('p'));
+				if ($halaman) {
+
+					if (file_exists(APPPATH . "views/pages/$halaman.php"))
+						$this->load->view("pages/$halaman");
+					else
+
+						echo "
+           <div class='container-fluid dashboard-default-sec'>
+              <div class='row'>
+                <div class='col-xl-12'> 
+                  <div  class='alert bg-white row text-dark'>
+                    <div class='col-md-6 p-4'>
+                        <h3 class='text-danger'><i class='fa fa-exclamation-triangle'></i> ERROR 404</h3>
+                        <hr> 
+                        Halaman tidak tersedia.
+                        Beberapa Penyebab Halaman ini tidak bisa ditampilkan, antara lain:
+                        <ol>
+                        <li>Halaman ini belum dikembangkan</li>
+                        <li>Kesalahan alamat permohonan akses halaman </li>
+                        <li>Kesalahan jaringan</li>
+                        </ol> 
+                        silahkan kunjungi halaman ini dilain waktu.<br> 
+                        Terimakasih
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+                  ";
+					// echo "[ERROR 404] Halaman tidak ditemukan";
+
+				} else
+					$this->load->view("pages/dashboard");
+				?>
+			</div>
+
+			<footer class="footer pt-3  ">
+				<div class="container-fluid">
+					<div class="row align-items-center justify-content-lg-between">
+						<div class="col-lg-6 mb-lg-0 mb-4">
+							<div class="copyright text-center text-sm text-muted text-lg-start">
+								© <script>
+									document.write(new Date().getFullYear())
+								</script>,
+								made with <i class="fa fa-heart"></i> by
+								<a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
+								for a better web.
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<ul class="nav nav-footer justify-content-center justify-content-lg-end">
+								<li class="nav-item">
+									<a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
+								</li>
+								<li class="nav-item">
+									<a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
+								</li>
+								<li class="nav-item">
+									<a href="https://www.creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
+								</li>
+								<li class="nav-item">
+									<a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+	</main>
+	<div class="fixed-plugin">
+		<a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
+			<i class="fa fa-cog py-2"> </i>
+		</a>
+		<div class="card shadow-lg">
+			<div class="card-header pb-0 pt-3 ">
+				<div class="float-start">
+					<h5 class="mt-3 mb-0">Argon Configurator</h5>
+					<p>See our dashboard options.</p>
+				</div>
+				<div class="float-end mt-4">
+					<button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
+						<i class="fa fa-close"></i>
+					</button>
+				</div>
+				<!-- End Toggle Button -->
+			</div>
+			<hr class="horizontal dark my-1">
+			<div class="card-body pt-sm-3 pt-0 overflow-auto">
+				<!-- Sidebar Backgrounds -->
+				<div>
+					<h6 class="mb-0">Sidebar Colors</h6>
+				</div>
+				<a href="javascript:void(0)" class="switch-trigger background-color">
+					<div class="badge-colors my-2 text-start">
+						<span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
+						<span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
+						<span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
+						<span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
+						<span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
+						<span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
+					</div>
+				</a>
+				<!-- Sidenav Type -->
+				<div class="mt-3">
+					<h6 class="mb-0">Sidenav Type</h6>
+					<p class="text-sm">Choose between 2 different sidenav types.</p>
+				</div>
+				<div class="d-flex">
+					<button class="btn bg-gradient-primary w-100 px-3 mb-2 active me-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
+					<button class="btn bg-gradient-primary w-100 px-3 mb-2" data-class="bg-default" onclick="sidebarType(this)">Dark</button>
+				</div>
+				<p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
+				<!-- Navbar Fixed -->
+				<div class="d-flex my-3">
+					<h6 class="mb-0">Navbar Fixed</h6>
+					<div class="form-check form-switch ps-0 ms-auto my-auto">
+						<input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
+					</div>
+				</div>
+				<hr class="horizontal dark my-sm-4">
+				<div class="mt-2 mb-5 d-flex">
+					<h6 class="mb-0">Light / Dark</h6>
+					<div class="form-check form-switch ps-0 ms-auto my-auto">
+						<input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
+					</div>
+				</div>
+				<a class="btn bg-gradient-dark w-100" href="https://www.creative-tim.com/product/argon-dashboard">Free Download</a>
+				<a class="btn btn-outline-dark w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard">View documentation</a>
+				<div class="w-100 text-center">
+					<a class="github-button" href="https://github.com/creativetimofficial/argon-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/argon-dashboard on GitHub">Star</a>
+					<h6 class="mt-3">Thank you for sharing!</h6>
+					<a href="https://twitter.com/intent/tweet?text=Check%20Argon%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fargon-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
+						<i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
+					</a>
+					<a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/argon-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
+						<i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!--   Core JS Files   -->
+	<script src="<?= base_url('assets/js/core/popper.min.js') ?>"></script>
+	<script src="<?= base_url('assets/js/core/bootstrap.min.js"') ?>"></script>
+	<script src="<?= base_url('assets/js/plugins/perfect-scrollbar.min.js') ?>"></script>
+	<script src="<?= base_url('assets/js/plugins/smooth-scrollbar.min.js') ?>"></script>
+	<script src="<?= base_url('assets/js/plugins/chartjs.min.js') ?>"></script>
+	<script>
+		var ctx1 = document.getElementById("chart-line").getContext("2d");
+
+		var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
+
+		gradientStroke1.addColorStop(1, 'rgba(94, 114, 228, 0.2)');
+		gradientStroke1.addColorStop(0.2, 'rgba(94, 114, 228, 0.0)');
+		gradientStroke1.addColorStop(0, 'rgba(94, 114, 228, 0)');
+		new Chart(ctx1, {
+			type: "line",
+			data: {
+				labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+				datasets: [{
+					label: "Mobile apps",
+					tension: 0.4,
+					borderWidth: 0,
+					pointRadius: 0,
+					borderColor: "#5e72e4",
+					backgroundColor: gradientStroke1,
+					borderWidth: 3,
+					fill: true,
+					data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+					maxBarThickness: 6
+
+				}],
+			},
+			options: {
+				responsive: true,
+				maintainAspectRatio: false,
+				plugins: {
+					legend: {
+						display: false,
+					}
+				},
+				interaction: {
+					intersect: false,
+					mode: 'index',
+				},
+				scales: {
+					y: {
+						grid: {
+							drawBorder: false,
+							display: true,
+							drawOnChartArea: true,
+							drawTicks: false,
+							borderDash: [5, 5]
+						},
+						ticks: {
+							display: true,
+							padding: 10,
+							color: '#fbfbfb',
+							font: {
+								size: 11,
+								family: "Open Sans",
+								style: 'normal',
+								lineHeight: 2
+							},
+						}
+					},
+					x: {
+						grid: {
+							drawBorder: false,
+							display: false,
+							drawOnChartArea: false,
+							drawTicks: false,
+							borderDash: [5, 5]
+						},
+						ticks: {
+							display: true,
+							color: '#ccc',
+							padding: 20,
+							font: {
+								size: 11,
+								family: "Open Sans",
+								style: 'normal',
+								lineHeight: 2
+							},
+						}
+					},
+				},
+			},
+		});
+	</script>
+	<?php if ($this->input->get('p')) { ?>
+		<script>
+			var origin = "<?= base_url() ?>";
+		</script>
+		<script src="<?= base_url("assets/js/pages/" . base64_decode($this->input->get('p')) . ".js") ?>"></script>
+	<?php } ?>
+	<script>
+		var win = navigator.platform.indexOf('Win') > -1;
+		if (win && document.querySelector('#sidenav-scrollbar')) {
+			var options = {
+				damping: '0.5'
+			}
+			Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+		}
+	</script>
+	<!-- Github buttons -->
+	<script async defer src="https://buttons.github.io/buttons.js"></script>
+	<!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+	<script src="<?= base_url("pages/" . $this->input->get('p')) . ".js" ?>"></script>
+	<script src="<?= base_url('assets/js/argon-dashboard.min.js') ?> "></script>
+</body>
---
+++ application/views/head.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets/img/logos/Logo_LingkunganKU-1.png') ?>">
+    <link rel="icon" type="image/png" href="<?= base_url('assets/img/logos/Logo_LingkunganKU-1.png') ?>">
+    <title>
+        LingkunganKu
+    </title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+
+    <!-- CSS Files -->
+    <link id="pagestyle" rel="stylesheet" href="<?= base_url('assets/css/argon-dashboard.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/nucleo-icons.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/nucleo-svg.css') ?>">
+</head>
+
+</html>
